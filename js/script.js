5df99442189e64392d266b203f1deab1f62c6e18
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-04-01 14:17:02 +0200

    Modify js script (add functions getMoveName and displayResult)

--- js/script.js
+++ js/script.js
@@ -1,16 +1,47 @@
+function getMoveName(argMoveId) {
+    if (argMoveId == 1) {
+        return 'kamień';
+    } else if (argMoveId == 2) {
+        return 'papier';
+    } else if (argMoveId == 3) {
+        return 'nożyce';
+    }
+
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+}
+
+function displayResult(argComputerMove, argPlayerMove) {
+    if (argPlayerMove == 'nieznany ruch') {
+        printMessage('Nie wybrałeś właściwego ruchu!');
+    } else {
+        if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+            printMessage('Ty wygrywasz!');
+        } else if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+            printMessage('Ty wygrywasz!');
+        } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+            printMessage('Ty wygrywasz!');
+        } else if (argPlayerMove == argComputerMove) {
+            printMessage('Remis!');
+        } else {
+            printMessage('Ja wygrywam!');
+        }
+    }
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber)
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if (randomNumber == 1) {
+/*if (randomNumber == 1) {
     computerMove = 'kamień';
 } else if (randomNumber == 2) {
     computerMove = 'papier';
 } else if (randomNumber == 3) {
     computerMove = 'nożyce';
-}
+}*/
 
 printMessage('Mój ruch to: ' + computerMove);
 
@@ -18,30 +49,16 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(playerInput);
 
-if (playerInput == '1') {
+/*if (playerInput == '1') {
     playerMove = 'kamień';
 } else if (playerInput == '2') {
     playerMove = 'papier';
 } else if (playerInput == '3') {
     playerMove = 'nożyce';
-}
+}*/
 
 printMessage('Twój ruch to: ' + playerMove);
 
-if (playerMove == 'nieznany ruch') {
-    printMessage('Nie wybrałeś właściwego ruchu!');
-} else {
-    if (playerMove == 'kamień' && computerMove == 'nożyce') {
-        printMessage('Ty wygrywasz!');
-    } else if (playerMove == 'papier' && computerMove == 'kamień') {
-        printMessage('Ty wygrywasz!');
-    } else if (playerMove == 'nożyce' && computerMove == 'papier') {
-        printMessage('Ty wygrywasz!');
-    } else if (playerMove == computerMove) {
-        printMessage('Remis!');
-    } else {
-        printMessage('Ja wygrywam!');
-    }
-}
+displayResult(computerMove, playerMove);
